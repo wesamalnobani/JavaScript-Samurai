947186a538870f5a51b9ec302b47c442966742cb
code.walnoubani <wesam.alnobani@gmail.com>
2019-07-14 10:55:37 +0300

    14072019

---
+++ 39- Named Parameters/app.js
@@ -0,0 +1,10 @@
+function f(o) {
+    let {x, y} = o
+
+    console.log(x, y)
+}
+
+f({
+    y:5,
+    x:2
+})
---
+++ 40- Numbers/app.js
@@ -0,0 +1,16 @@
+/*
+5
+3.6
+number
+
+5465
+exponential
+    312.69 ==>(1:10) * 10*n
+    312.69 ==> 3.1269 * 10^+2
+*/
+
+let x = 3.1269e+2
+console.log(x)
+
+let y = 0x12
+console.log(y)
