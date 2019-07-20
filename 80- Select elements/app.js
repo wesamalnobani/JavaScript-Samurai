d89f29d23a65281f9a8381a3005b56b481a37ceb
code.walnoubani <wesam.alnobani@gmail.com>
2019-07-20 03:16:48 +0300

    20072019

---
+++ 80- Select elements/app.js
@@ -0,0 +1,14 @@
+let e = document.getElementById('myDive')
+console.log(e)
+
+
+let s = document.getElementsByClassName('myDiveClass')
+console.log(s)
+
+
+let r = document.getElementsByTagName('div')
+console.log(r)
+
+let t = document.querySelector('div.myDivClass')
+let u = document.querySelectorAll('div.myDivClass')
+
---
+++ 86- Create Nodes/app.js
@@ -0,0 +1,10 @@
+classList
+classList.add()
+classList.remove()
+classList.toggle()
+classList.contains()
+classList.replace()
+classList.length
+classList.array.forEach(element => {
+    
+});
