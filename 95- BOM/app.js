7142edeb26141f96ef391dd135d2a4155d85059e
code.walnoubani <wesam.alnobani@gmail.com>
2019-07-27 02:02:03 +0300

    27072019

---
+++ 95- BOM/app.js
@@ -0,0 +1,20 @@
+scrollX
+scrollY
+
+
+// scrollBy(x, y)
+scrollBy(0, 20)
+
+// scrollTo(x, y)
+scrollTo(0, 50)
+
+// alert()
+alert('Hello From Alert')
+
+// confirm() ==> True , Fales
+confirm('Are you Sure')
+
+//prompt('String', Defualt Value)
+prompt('What is your Name',)
+
+
